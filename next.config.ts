8c2a41ffc397554d1f6030ef4e4b563e07a4ebbe
fethulmubin <fetihul@gmail.com>
2025-08-26 18:27:08 +0300

    next cofing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",               // when frontend calls /api/...
+        destination: "https://talenthub-api-1.onrender.com/:path*", // proxy to backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
