8dd9c40e605c3069a65c7e9fb03ef306b6ff7671
fethulmubin <fetihul@gmail.com>
2025-09-15 13:48:09 +0300

    seo added and og images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,38 @@ const geistMono = Geist_Mono({
 });
 
 // app/layout.tsx
-export const metadata : Metadata = {
+export const metadata: Metadata = {
   title: "TalentHub - Mini Job Portal",
+  description: "TalentHub is a modern mini job portal to apply, track, and manage job applications with ease.",
   icons: {
-    icon: "/talent.png",
+    icon: [
+      { url: "/talent.png", sizes: "32x32", type: "image/png" }, // browser tab
+      { url: "/talent.png", sizes: "192x192", type: "image/png" }, // android / chrome apps
+    ],
+    apple: [
+      { url: "/talent.png", sizes: "180x180", type: "image/png" }, // iOS
+    ],
+  },
+  openGraph: {
+    title: "TalentHub - Mini Job Portal",
+    description: "Find and apply for jobs easily with TalentHub.",
+    url: "https://talent-hub-front-cckb.vercel.app/",
+    siteName: "TalentHub",
+    images: [
+      {
+        url: "/talent.png",
+        width: 1200,
+        height: 630,
+        alt: "TalentHub Logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "TalentHub - Mini Job Portal",
+    description: "Apply, track, and manage jobs with ease using TalentHub.",
+    images: ["/talent.png"],
   },
 };
 
